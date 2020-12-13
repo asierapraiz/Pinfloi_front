a2413497b10c070e849cb593f8b898cf115aa1f0
asier <asierapraiz@gmail.com>
2020-12-13 13:43:44 +0100

    Angular 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -75,7 +75,7 @@ const routes: Routes = [
     BrowserModule,
     HttpClientModule,
     FormsModule,    
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     BrowserAnimationsModule, MatDatepickerModule, MatMomentDateModule,
     ReactiveFormsModule, MatAutocompleteModule, MatInputModule, MatFormFieldModule, FontAwesomeModule
   ],
